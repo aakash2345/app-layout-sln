adeb5350cce678ed0881cabe81dfec180b475751
Aakash Nethi <aakash.nethi@parker.com>
2018-08-30 09:53:30 -0400

    Add project files.

---
+++ PHconnect-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PHconnect_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
